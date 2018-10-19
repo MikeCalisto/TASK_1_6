f639e7ad89f99b164bc46ba3a04729126c0606c2
MikeCalisto <m.zemlyanie@gmail.com>
2018-10-19 21:50:55 +0300

    rt

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Михаил Земляной");
+        System.out.println("НВК гимназия №11");
+        System.out.println("10-Б класс");
+    }
+}
